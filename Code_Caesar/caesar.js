b3d4804430b625797ca30f80b701059c873c552c
Gagnant Nicolas <gagnantnicolas64@gmail.com>
2019-05-14 12:05:35 +0200

    rendu nizar_nico

---
+++ Code_Caesar/caesar.js
@@ -0,0 +1,70 @@
+// Créer un tableau, puis avec une condition ont appel une lettre qui est strictement égal à la valeur du tableau * 3 et on appel cette nouvelle variable
+
+const tableauClair = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z"
+];
+
+const tableauChiffree = [
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "A",
+  "B",
+  "C"
+];
+
+let essai = ["B", "O", "N", "J", "O", "U", "R"];
+
+for (essai = 0; essai < 25; essai++) {
+  for (tableauClair = 0; tableauClair < 25; tableauClair++) {
+    if (tableauClair) {
+    } else {
+      console.log("ERROR");
+    }
+  }
+}
